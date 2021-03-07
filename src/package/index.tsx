aefdf1fc2de223d0cd6f6f9d74376a076c4f0b3c
rpfeifer <rene_pfeifer@kabelbw.de>
2021-03-07 15:09:17 +0100

    Export new control.

--- src/package/index.tsx
+++ src/package/index.tsx
@@ -1,3 +1,4 @@
-export { default as AuthCode } from './authcode';
-export { default as Autocomplete } from './autocomplete';
-export { default as Alert } from './alert';
+export { default as AuthCode } from "./authcode";
+export { default as Autocomplete } from "./autocomplete";
+export { default as Alert } from "./alert";
+export { default as DateTimeField } from "./date";
