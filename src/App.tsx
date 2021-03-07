33a54285a3754389580e83ff10ec32511757b6b8
rpfeifer <rene_pfeifer@kabelbw.de>
2021-03-07 10:15:27 +0100

    Change control framework.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+/** @format */
+
+import React, { useEffect } from 'react';
 import './App.css';
 import { observer } from 'mobx-react';
 import { Button, createMuiTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@material-ui/core';
@@ -10,30 +12,51 @@ interface ThemeProps {
 
 }
 
-const Theme = observer(({children}: React.PropsWithChildren<ThemeProps>) => {
+const Theme = observer(({ children }: React.PropsWithChildren<ThemeProps>) => {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const paletteType = prefersDarkMode ? 'dark' : 'light';
+  const [darkMode, setDarkMode] = React.useState(localStorage.getItem('dark') === 'true');
+  useEffect(() => {
+    if (localStorage.getItem('dark') === null) {
+      setDarkMode(prefersDarkMode)
+    }
+  }, [prefersDarkMode]);
+  const paletteType = darkMode ? 'dark' : 'light';
   const theme = React.useMemo(
-    () => 
-    createMuiTheme({
-      palette: {
-        type: paletteType,
-        primary: {
-          main: '#90caf9'
-        },
-        secondary: {
-          main: '#f48fb1'
+    () =>
+      createMuiTheme({
+        palette: {
+          type: paletteType,
+          primary: {
+            main: '#90caf9'
+          },
+          secondary: {
+            main: '#f48fb1'
+          }
         }
-      }
-    }),
+      }),
     [paletteType]
   );
+  const toggleDark = () => {
+    setDarkMode(!darkMode);
+    localStorage.setItem('dark', darkMode ? 'false' : 'true');
+  }
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <div style={{border: '1px solid #888', padding: 8}}>
-        {children}
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div style={{ border: '1px solid #888', padding: 8 }}>
+          <Button
+            onClick={() => toggleDark()}
+            style={{
+              position: 'absolute',
+              right: 0,
+              top: 0
+            }}
+          >{darkMode ? 'Dark' : 'Light'}
+          </Button>
+          {children}
+        </div>
+      </React.Fragment>
     </ThemeProvider>
   )
 });
@@ -46,18 +69,18 @@ function App() {
   }
   return (
     <Theme>
-      {hash !== '#autocomplete' || <AutoCompleteTest/>}
-      {hash !== '#authcode' || <AuthCodeTest/>}
-      {hash !== '#alert' || <AlertTest/>}
+      {hash !== '#autocomplete' || <AutoCompleteTest />}
+      {hash !== '#authcode' || <AuthCodeTest />}
+      {hash !== '#alert' || <AlertTest />}
       {!hash ? (
         <React.Fragment>
           <Button onClick={() => goto('#autocomplete')}>Autocomplete</Button>
           <Button onClick={() => goto('#authcode')}>AuthCode</Button>
           <Button onClick={() => goto('#alert')}>Alert</Button>
         </React.Fragment>
-      ): (
-        <Button onClick={() => goto('')} style={{position:'absolute', top: 8, left: 8 }}> &lt; Back</Button>
-      )}
+      ) : (
+          <Button onClick={() => goto('')} style={{ position: 'absolute', top: 8, left: 8 }}> &lt; Back</Button>
+        )}
     </Theme>
   );
 }
