4730213e2c2f4671648ed4567b9e28061d4ce81d
rpfeifer <rene_pfeifer@kabelbw.de>
2021-04-02 19:10:14 +0200

    Export address.

--- src/package/index.tsx
+++ src/package/index.tsx
@@ -1,5 +1,6 @@
 export { default as AuthCode, useAuthCodeRef } from "./authcode";
 export { default as Alert } from "./alert";
+export { default as Address } from "./address";
 export { default as Date, useDateRef } from "./date";
 export { default as Select, useSelectRef } from "./select";
 export { default as UIContext, useUIContext } from "./UIContext";
