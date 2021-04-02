db6e84447e7bdd5bdb1418f396f67d976b382706
rpfeifer <rene_pfeifer@kabelbw.de>
2021-04-02 14:20:25 +0200

    Export new functions.

--- src/package/index.tsx
+++ src/package/index.tsx
@@ -2,3 +2,5 @@ export { default as AuthCode, useAuthCodeRef } from "./authcode";
 export { default as Alert } from "./alert";
 export { default as Date, useDateRef } from "./date";
 export { default as Select, useSelectRef } from "./select";
+export { default as UIContext, useUIContext } from "./UIContext";
+export { default as photonParser } from "./address/photonParser";
